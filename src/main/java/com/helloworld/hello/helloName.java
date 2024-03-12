1c2fc31a3ad7349d9c7eaa0e788d7d3d506f9cf2
Gai-Tosaka <tosakagai777@gmail.com>
2024-03-12 20:54:40 +0900

    save actionを使って保存時にフォーマットを編集

--- src/main/java/com/helloworld/hello/helloName.java
+++ src/main/java/com/helloworld/hello/helloName.java
@@ -2,9 +2,11 @@ package com.helloworld.hello;
 
 public class HelloName {
     private String name;
+
     public HelloName(String name) {
         this.name = name + "さん！　こんにちは！";
     }
+    
     public String getName() {
         return name;
     }
