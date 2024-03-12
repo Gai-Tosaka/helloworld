6ffd01d33c528f153e572ec38ea31ecee7a4fb64
Gai-Tosaka <tosakagai777@gmail.com>
2024-03-12 21:07:05 +0900

    エンドポイントを複数形に変更

--- src/main/java/com/helloworld/hello/helloController.java
+++ src/main/java/com/helloworld/hello/helloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/hellos")
     public HelloName hello() {
         return new HelloName("サンタクロース");
     }
